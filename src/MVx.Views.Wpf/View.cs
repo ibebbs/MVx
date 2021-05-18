27b463f22142495bcf4271374f68f18e78ba82ed
ibebbs <github@bebbs.co.uk>
2021-05-18 12:19:53 +0100

    Added MVx.Views.Wpf

---
+++ src/MVx.Views.Wpf/View.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVx.Views.Wpf
+{
+    public static class View
+    {
+        public static readonly DependencyProperty ModelProperty = DependencyProperty.RegisterAttached("Model", typeof(object), typeof(View), new PropertyMetadata(null, ModelPropertyChanged));
+
+        private static void ModelPropertyChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue != null)
+            {
+                var view = ViewLocator.Instance.LocateForModel(e.NewValue, target);
+
+                view.DataContext = e.NewValue;
+
+                SetContentProperty(target, view);
+            }
+            else
+            {
+                SetContentProperty(target, e.NewValue);
+            }
+        }
+
+
+        private static bool SetContentProperty(ContentControl contentControl, object newValue)
+        {
+            contentControl.Content = newValue;
+
+            return true;
+        }
+
+        private static bool SetContentProperty(Control control, object newValue)
+        {
+            var property = control.GetType().GetProperty("Content");
+
+            if (property != null)
+            {
+                property.SetValue(control, newValue);
+
+                return true;
+            }
+            else
+            {
+                throw new ArgumentException($"Could not find content property on type: '{control.GetType().Name}'", nameof(control));
+            }
+        }
+
+        private static bool SetContentProperty(DependencyObject target, object newValue)
+        {
+            return target switch
+            {
+                ContentControl cc => SetContentProperty(cc, newValue),
+                Control c => SetContentProperty(c, newValue),
+                _ => throw new ArgumentException($"Unknown target type: '{target.GetType().Name}'", nameof(target))
+            };
+        }
+
+        public static object GetModel(DependencyObject obj)
+        {
+            return (object)obj.GetValue(ModelProperty);
+        }
+
+        public static void SetModel(DependencyObject obj, object value)
+        {
+            obj.SetValue(ModelProperty, value);
+        }
+    }
+}
---
+++ src/MVx.Views.Wpf/ViewLocator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVx.Views.Wpf
+{
+    /// <summary>
+    /// Represents a component that can locate the view for a given model
+    /// </summary>
+    public interface IViewLocator
+    {
+        /// <summary>
+        /// Get the view for the specified <paramref name="model"/>, optionally
+        /// determined by where it is being displayed provided by <paramref name="target"/>
+        /// </summary>
+        /// <param name="model">The model to get the view for</param>
+        /// <param name="target">The location the view will be displayed</param>
+        /// <returns></returns>
+        FrameworkElement LocateForModel(object model, DependencyObject target);
+    }
+
+    /// <summary>
+    /// Provides a default implementation of the <see cref="IViewLocator"/> interface
+    /// </summary>
+    /// <remarks>
+    /// To use different conventions, replace the value of the <see cref="Instance"/> property with
+    /// a new implementation of <see cref="IViewLocator"/>
+    /// </remarks>
+    public class ViewLocator : IViewLocator
+    {
+        public static IViewLocator Instance { get; set; } = new ViewLocator();
+
+        /// <summary>
+        /// Get the view for the specified <paramref name="model"/>, optionally
+        /// determined by where it is being displayed provided by <paramref name="target"/>
+        /// </summary>
+        /// <param name="model">The model to get the view for</param>
+        /// <param name="target">The location the view will be displayed</param>
+        public FrameworkElement LocateForModel(object model, DependencyObject target)
+        {
+            var viewTypeName = model.GetType().FullName.Replace("Model", string.Empty);
+
+            var viewType = Type.GetType(viewTypeName, false);
+
+            if (viewType != null)
+            {
+                return Activator.CreateInstance(viewType) as FrameworkElement;
+            }
+            else
+            {
+                return new ContentControl();
+            }
+        }
+    }
+}
